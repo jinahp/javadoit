334d003eb9f35bbcb50ccd2c8a0a6f0aec734088
jinah <monsatfri@gmail.com>
2022-09-26 17:06:15 +0900

    필드 - 클래스 내부 구성 요소

---
+++ field/src/sec01_field/EX02_InitialValueOfFieldAndLocalVariable/InitialValueOfFieldAndLocalVariable.java
@@ -0,0 +1,26 @@
+package sec01_field.EX02_InitialValueOfFieldAndLocalVariable;
+
+
+class A {
+	boolean m1;
+	int m2;
+	double m3;
+	String m4;
+	void printFieldValue() {
+		System.out.println(m1);
+		System.out.println(m2);
+		System.out.println(m3);
+		System.out.println(m4);
+	}
+	void printLocalVariable() {
+		int k;
+	}
+}
+public class InitialValueOfFieldAndLocalVariable {
+	public static void main(String[] args) {
+
+		A a = new A();
+		
+		a.printFieldValue();
+	}
+}
